d71e7d78a4e4b4564b5222069d052bd81d9fa838
vzrg-tamu <vzrg.tamu@gmail.com>
2023-05-16 17:16:24 -0500

    Raster update

--- autocommit.bat
+++ autocommit.bat
@@ -1,3 +1,4 @@
-git add .                          REM :: add all files of the current directory to track
-git commit -m "Raster update"      REM :: commit all tracked files
-git push                           REM :: Branch (i.e. master)
+cd C:\Users\vinit.sehgal\Documents\GitHub\FLASH
+git add .                       REM :: Add all files of the current directory to track
+git commit -m "Raster update"   REM :: Commit all tracked files
+git push                        REM :: Branch (i.e. master)
