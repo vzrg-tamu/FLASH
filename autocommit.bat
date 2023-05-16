886595fb556295f87bf064ee622424be2dbae457
vzrg-tamu <vzrg.tamu@gmail.com>
2023-05-16 17:19:55 -0500

    Delete autocommit.bat

--- autocommit.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Users\vinit.sehgal\Documents\GitHub\FLASH
-git add .                       REM :: Add all files of the current directory to track
-git commit -m "Raster update"   REM :: Commit all tracked files
-git push                        REM :: Branch (i.e. master)
