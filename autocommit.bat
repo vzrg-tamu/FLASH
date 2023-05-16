acadabffa81086d93a07c11b914c7d81883903a0
vzrg-tamu <vzrg.tamu@gmail.com>
2023-05-16 10:51:33 -0500

    Raster update

--- autocommit.bat
+++ autocommit.bat
@@ -1,3 +1,3 @@
 git add .                          REM :: add all files of the current directory to track
 git commit -m "Raster update"      REM :: commit all tracked files
-git push origin master             REM :: Branch (i.e. master)
+git push                           REM :: Branch (i.e. master)
